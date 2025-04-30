97db59b769fd4bcd5abda2ac274fe052b9f9da0e
sathwika <sathwikasweety32@gmail.com>
2025-04-30 14:06:11 +0530

    Updated backend files and added .gitignore

--- Backend/src/main/java/com/example/fakenewsdetection/FakenewsdetectionApplication.java
+++ Backend/src/main/java/com/example/fakenewsdetection/FakenewsdetectionApplication.java
@@ -4,11 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.fakenewsdetection")
 public class FakenewsdetectionApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("google.factcheck.api.key", dotenv.get("GOOGLE_FACTCHECK_API_KEY"));
 		SpringApplication.run(FakenewsdetectionApplication.class, args);
 	}
 
